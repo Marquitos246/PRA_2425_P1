b4e477536ddad20acfbe934cf942e04ed9156db5
Marcos Anaya Benavente <manaben@upv.edu.es>
2025-11-06 08:40:12 +0100

    Añadida interfaz genérica List

---
+++ List.h
@@ -0,0 +1,14 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdexcept>
+
+template <typename T> 
+class List {
+    public:
+        virtual ~List() {}
+
+	virtual void insert(int pos, T e) = 0;
+};
+
+#endif
