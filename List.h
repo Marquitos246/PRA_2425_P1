3c8f1ddd56826e23578a311c606989f1e11ef923
Marcos Anaya Benavente <manaben@upv.edu.es>
2025-11-06 09:01:53 +0100

    Añadida interfaz genérica List

--- List.h
+++ List.h
@@ -9,6 +9,13 @@ class List {
         virtual ~List() {}
 
 	virtual void insert(int pos, T e) = 0;
+	virtual void append(T e) = 0;
+	virtual void prepend(T e) = 0;
+	virtual T remove(int pos) = 0;
+	virtual const T get(int pos) = 0;
+	virtual const int search(T e) = 0;
+	virtual const bool empty() = 0;
+	virtual const int size() = 0;
 };
 
 #endif
